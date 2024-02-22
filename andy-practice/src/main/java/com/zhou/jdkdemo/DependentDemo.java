78349e99b31065dc71368365eaebdad8d99939c1
zhouyuanke <yuanke.zhou@delonixmail.com>
2024-02-22 11:11:24 +0800

    2023-02-22

--- andy-practice/src/main/java/com/zhou/jdkdemo/DependentDemo.java
+++ andy-practice/src/main/java/com/zhou/jdkdemo/DependentDemo.java
@@ -3,7 +3,6 @@ package com.zhou.jdkdemo;
 import org.springframework.stereotype.Component;
 
 /**
- * @author: yuanke.zhou@delonixmail.com
  * @createTime: 2024-01-27 19:55
  * @description: 依赖demo
  */
--- andy-practice/src/main/java/com/zhou/jdkdemo/JdkDemo.java
+++ andy-practice/src/main/java/com/zhou/jdkdemo/JdkDemo.java
@@ -3,7 +3,6 @@ package com.zhou.jdkdemo;
 import org.springframework.stereotype.Component;
 
 /**
- * @author: yuanke.zhou@delonixmail.com
  * @createTime: 2024-01-05 08:44
  * @description: jdkDemo类
  */
